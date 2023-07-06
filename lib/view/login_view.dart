ee93360f2f9c70a4ff7b1bde71897456f935f8b7
Gabriel Jose <gabrieljose528@gmail.com>
2023-07-06 20:19:19 -0300

    Adiciona arquivos do projeto

---
+++ lib/view/login_view.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                height: height * .30,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF583290),
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(16)),
+                ),
+                child: Center(
+                  child: Image.asset('assets/images/parkflow_logo.png'),
+                ),
+              ),
+              Container(
+                height: height * .70,
+                color: Colors.white,
+              )
+            ],
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding:
+                EdgeInsets.only(top: height * .22, right: 20.0, left: 20.0),
+            child: Container(
+              height: 200.0,
+              width: width,
+              child: const Card(
+                color: Colors.white,
+                elevation: 4.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
