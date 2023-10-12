4c3a8523741c8f83ba7dc769a546399fc0e0f920
Gabriel Jose <gabrieljose528@gmail.com>
2023-10-12 17:53:07 -0300

    Adiciona Card para quando as listas estiverem vazias

---
+++ lib/view/widgets/default_card_message.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DefaultCardMessage extends StatelessWidget {
+  final String message;
+
+  const DefaultCardMessage({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        elevation: 3,
+        child: ClipPath(
+          clipper: ShapeBorderClipper(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.10,
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Center(
+              child: Text(
+                message,
+                style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF35244E)),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
