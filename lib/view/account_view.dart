3cf4325a3c152a78909a61a1cd62342d9810bc5a
Gabriel Jose <gabrieljose528@gmail.com>
2023-10-21 15:06:39 -0300

    Ajusta formulário da tela "Minha conta"

--- lib/view/account_view.dart
+++ lib/view/account_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:parkflow_app/view/widgets/default_text_field.dart';
+import 'package:parkflow_app/view/widgets/masked_text_field.dart';
+
+import '../utils/regex.dart';
 
 class AccountView extends StatefulWidget {
   const AccountView({super.key});
@@ -9,8 +13,10 @@ class AccountView extends StatefulWidget {
 }
 
 class _AccountViewState extends State<AccountView> {
-  TextEditingController _defaultController = TextEditingController();
-  
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,31 +41,16 @@ class _AccountViewState extends State<AccountView> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
+                      Text(
                         'Minha conta',
                         style: TextStyle(
                             color: Color(0xFF35244E),
                             fontSize: 24,
                             fontWeight: FontWeight.bold),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            const Color(0xFFC45454),
-                          ),
-                        ),
-                        child: const Row(
-                          children: [
-                            Icon(Icons.person_remove),
-                            SizedBox(width: 9),
-                            Text('Desativar conta')
-                          ],
-                        ),
-                      ),
+                      )
                     ],
                   ),
                   const SizedBox(
@@ -76,81 +67,95 @@ class _AccountViewState extends State<AccountView> {
                       child: Padding(
                         padding: const EdgeInsets.symmetric(
                             horizontal: 20, vertical: 30),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Nome',
-                              style: TextStyle(
-                                  color: Color(0xFF4F2D82),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(
-                              height: 7,
-                            ),
-                            DefaultTextField(
-                              formController: _defaultController,
-                              icon: Icons.person,
-                              hintText: 'Gabriel da Silva Toledo',
-                              isPasswordField: false,
-                            ),
-                            const SizedBox(
-                              height: 16,
-                            ),
-                            const Text(
-                              'Email',
-                              style: TextStyle(
-                                  color: Color(0xFF4F2D82),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(
-                              height: 7,
-                            ),
-                            DefaultTextField(
-                              formController: _defaultController,
-                              icon: Icons.mail,
-                              hintText: 'parkflow@email.com',
-                              isPasswordField: false,
-                            ),
-                            const SizedBox(
-                              height: 16,
-                            ),
-                            const Text(
-                              'Telefone',
-                              style: TextStyle(
-                                  color: Color(0xFF4F2D82),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(
-                              height: 7,
-                            ),
-                            DefaultTextField(
-                              formController: _defaultController,
-                              icon: Icons.phone,
-                              hintText: '(16) 99999-9999',
-                              isPasswordField: false,
-                            ),
-                            const SizedBox(
-                              height: 26,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text(
-                                      'Salvar alterações',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16),
+                        child: Form(
+                          key: formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Nome',
+                                style: TextStyle(
+                                    color: Color(0xFF4F2D82),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 7,
+                              ),
+                              SizedBox(
+                                child: DefaultTextField(
+                                  formController: _nameController,
+                                  hintText: 'Gabriel da Silva Toledo',
+                                  icon: Icons.person,
+                                  isPasswordField: false,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Preencha este campo';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              const Text(
+                                'Telefone',
+                                style: TextStyle(
+                                    color: Color(0xFF4F2D82),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 7,
+                              ),
+                              SizedBox(
+                                child: MaskedTextField(
+                                  controller: _phoneController,
+                                  text: '(99) 99999-9999',
+                                  icon: Icons.phone,
+                                  isPasswordField: false,
+                                  keyboardType: TextInputType.phone,
+                                  maskFormatter: MaskTextInputFormatter(
+                                    mask: '(##) #####-####',
+                                    filter: {"#": RegExp(r'[0-9]')},
+                                    type: MaskAutoCompletionType.lazy,
+                                  ),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Preencha este campo';
+                                    }
+                                    if (!RegExp(Regex.phone).hasMatch(value)) {
+                                      return 'Número de telefone inválido';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 26,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        FocusManager.instance.primaryFocus
+                                            ?.unfocus();
+                                        if (formKey.currentState!.validate()) {
+                                          print('form alter valid');
+                                        }
+                                      },
+                                      child: const Text(
+                                        'Salvar alterações',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ],
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
